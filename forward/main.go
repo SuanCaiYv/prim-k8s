c523faaf1b1b81fa7d27ffdf833bc4355ab51dfb
SuanCaiYv <jokerblack2508826394@gmail.com>
2023-09-14 18:09:45 +0800

    增加实现

--- forward/main.go
+++ forward/main.go
@@ -1,63 +1,28 @@
 package main
 
 import (
-	"io"
+	"fmt"
 	"net"
 	"os"
-	"strconv"
 )
 
 func main() {
-	targetIP := os.Getenv("TARGET_IP")
-	targetPort0 := os.Getenv("TARGET_PORT")
-	protocol := os.Getenv("PROTOCOL")
-	bindPort := 8190
-	targetPort, _ := strconv.Atoi(targetPort0)
-	if protocol == "TCP" {
-		listener, err := net.ListenTCP("tcp", &net.TCPAddr{
-			IP:   net.IPv4zero,
-			Port: bindPort,
-		})
-		if err != nil {
-			panic(err)
-		}
-		for {
-			conn, err := listener.Accept()
-			if err != nil {
-				panic(err)
-			}
-			go func() {
-				targetConn, err := net.DialTCP("tcp", nil, &net.TCPAddr{
-					IP:   net.ParseIP(targetIP),
-					Port: targetPort,
-				})
-				if err != nil {
-					panic(err)
-				}
-				go forward(conn, targetConn)
-				go forward(targetConn, conn)
-			}()
-		}
-	} else {
-		listener, err := net.ListenUDP("udp", &net.UDPAddr{
-			IP:   net.IPv4zero,
-			Port: bindPort,
-		})
-		if err != nil {
-			panic(err)
-		}
-		targetConn, err := net.DialUDP("udp", nil, &net.UDPAddr{
-			IP:   net.ParseIP(targetIP),
-			Port: targetPort,
-		})
-		if err != nil {
-			panic(err)
-		}
-		go forward(listener, targetConn)
-		forward(targetConn, listener)
+	listener, err := net.ListenPacket("udp", "0.0.0.0:8190")
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
 	}
-}
-
-func forward(reader io.Reader, writer io.Writer) {
-	io.Copy(writer, reader)
+	for {
+		buf := make([]byte, 1024)
+		n, addr, err := listener.ReadFrom(buf)
+		if err != nil {
+			fmt.Println(err)
+			continue
+		}
+		req := buf[:n]
+		fmt.Println(addr)
+		hostname, _ := os.Hostname()
+		resp := fmt.Sprintf("I'm udp server running on %s, you have request for %s", hostname, string(req))
+		_, _ = listener.WriteTo([]byte(resp), addr)
+	}
 }
